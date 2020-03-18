c22a215520a000f32d8a7dbe921b447633ccdbb2
aihong-mg <62324015+aihong-mg@users.noreply.github.com>
2020-03-18 14:39:14 +0800

    Add files via upload

---
+++ bizpage_files/global.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    var a = $(window);
+    setRootFontSize = function() {
+        var c = a.width();
+        var b;
+        if (c < 320) {
+            b = 20
+        } else {
+            if (640 <= c && c < 1200) {
+                b = c * 40 / 1200
+            } else {
+                if (1200 <= c) {
+                    b = 40
+                }
+            }
+        }
+        $("html").css("font-size", b + "px")
+    };
+    a.resize(setRootFontSize).load(setRootFontSize).resize()
+});
